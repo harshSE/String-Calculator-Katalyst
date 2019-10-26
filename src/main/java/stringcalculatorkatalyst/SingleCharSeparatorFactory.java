feaf170cf51384ac0046d2eb3ed209cbda369550
harshSE <h.rsh89@gmail.com>
2019-10-27 03:12:37 +0530

    STEP 8.20,  RED signal, adding test case file

---
+++ src/main/java/stringcalculatorkatalyst/SingleCharSeparatorFactory.java
@@ -0,0 +1,4 @@
+package stringcalculatorkatalyst;
+
+public class SingleCharSeparatorFactory implements CustomSeparatorPatternFactory {
+}
---
+++ src/test/java/stringcalculatorkatalyst/SingleBracketAndMultipleCharsSeparatorFactoryTest.java
@@ -0,0 +1,34 @@
+package stringcalculatorkatalyst;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.regex.Pattern;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class SingleBracketAndMultipleCharsSeparatorFactoryTest {
+    private SingleBracketAndMultipleCharsSeparatorFactory factory;
+
+    @Before
+    public void setUp() {
+        factory = new SingleBracketAndMultipleCharsSeparatorFactory();
+    }
+
+    public Object[][] parametersForCreatePatternFromProvidedInput() {
+        return new Object[][] {
+                {"[**]", Pattern.compile(",|\n|\\*\\*")},
+        };
+    }
+    @Test
+    @Parameters(method = "parametersForCreatePatternFromProvidedInput")
+    public void createPatternThatIncludeDefaultSeparatorFromProvidedInput(String string, Pattern pattern) {
+        assertThat(factory.createPattern(string).pattern(), is(equalTo(pattern.pattern())));
+    }
+}
