39294e684fbf4a6dd0b6af5bc90b05428767a4dc
harshSE <h.rsh89@gmail.com>
2019-10-26 17:10:37 +0530

    STEP 7.3, REFACTOR signal, separate method for split and create regx

--- src/main/java/stringcalculatorkatalyst/StringCalculator.java
+++ src/main/java/stringcalculatorkatalyst/StringCalculator.java
@@ -5,13 +5,16 @@ import java.util.HashSet;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
+import static java.util.Arrays.asList;
 import static java.util.Objects.isNull;
 
 public class StringCalculator {
     private Pattern pattern;
+    private HashSet<Character> escapeCharacters;
 
     public StringCalculator() {
         pattern = Pattern.compile("[,\n]");
+        escapeCharacters = new HashSet<>(asList('\\','^','*', '$','.','|','?','+','(',')','[',']','{','}'));
     }
     public int add(String stringNumber) throws IllegalArgumentException{
 
@@ -39,32 +42,47 @@ public class StringCalculator {
 
     private String[] split(String stringNumber) {
         String[] stringNumbers;
-        if(stringNumber.startsWith("//[")) {
-            int endIndexOfNewLineChar = stringNumber.indexOf("\n");
-            String customSeparator = stringNumber.substring(3, endIndexOfNewLineChar-1);
-            String substring = stringNumber.substring(endIndexOfNewLineChar+1);
-            HashSet<Character> escapeCharacters = new HashSet<>(Arrays.asList('\\','^','*'));
 
-            char customSeparatorChar = customSeparator.charAt(0);
-            if(escapeCharacters.contains(customSeparatorChar)) {
-                StringBuilder newCustomSeparator = new StringBuilder();
-                for(int index = 0; index < customSeparator.length(); index++) {
-                    newCustomSeparator.append("\\").append(customSeparatorChar);
-                }
-                customSeparator = newCustomSeparator.toString();
-            }
-            String regexString = ",|\n|" + customSeparator;
-            stringNumbers = substring.split(regexString);
+        if(stringNumber.startsWith("//[")) {
+            stringNumbers = splitWithArbitraryLengthOfCustomSeperator(stringNumber);
         } else if(stringNumber.startsWith("//")) {
-            int endIndexOfNewLineChar = stringNumber.indexOf("\\n");
-            String customSeparator = stringNumber.substring(2, endIndexOfNewLineChar);
-            String substring = stringNumber.substring(endIndexOfNewLineChar+2);
-            String regexString = ",\n" + customSeparator;
-            stringNumbers = substring.split("[" + regexString + "]");
+            stringNumbers = splitWithCustomSeparator(stringNumber);
         } else {
             stringNumbers = pattern.split(stringNumber);
         }
         return stringNumbers;
+    }
+
+    private String[] splitWithCustomSeparator(String stringNumber) {
+        String[] stringNumbers;
+        int endIndexOfNewLineChar = stringNumber.indexOf("\\n");
+        String customSeparator = stringNumber.substring(2, endIndexOfNewLineChar);
+        String substring = stringNumber.substring(endIndexOfNewLineChar+2);
+        String regexString = ",\n" + customSeparator;
+        stringNumbers = substring.split("[" + regexString + "]");
+        return stringNumbers;
+    }
+
+    private String[] splitWithArbitraryLengthOfCustomSeperator(String stringNumber) {
+        String[] stringNumbers;
+        int endIndexOfNewLineChar = stringNumber.indexOf("\n");
+        String customSeparator = stringNumber.substring(3, endIndexOfNewLineChar-1);
+        String substring = stringNumber.substring(endIndexOfNewLineChar+1);
+        String regexString = createRegexString(customSeparator);
+        stringNumbers = substring.split(regexString);
+        return stringNumbers;
+    }
+
+    private String createRegexString(String customSeparator) {
+        char customSeparatorChar = customSeparator.charAt(0);
+        if(escapeCharacters.contains(customSeparatorChar)) {
+            StringBuilder newCustomSeparator = new StringBuilder();
+            for(int index = 0; index < customSeparator.length(); index++) {
+                newCustomSeparator.append("\\").append(customSeparatorChar);
+            }
+            customSeparator = newCustomSeparator.toString();
+        }
+        return ",|\n|" + customSeparator;
     }
 
     private int toInt(String stringNumber) {
