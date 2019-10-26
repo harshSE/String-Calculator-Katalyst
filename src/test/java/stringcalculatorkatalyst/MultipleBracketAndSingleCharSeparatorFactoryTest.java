f5693cf14aba6c61458c3b27244d5bedeb77b65b
harshSE <h.rsh89@gmail.com>
2019-10-27 02:56:09 +0530

    STEP 8.16,  REFACTOR signal, adding more argument to getShouldReturnNullWhenNotMatchingProviderFound

---
+++ src/test/java/stringcalculatorkatalyst/MultipleBracketAndSingleCharSeparatorFactoryTest.java
@@ -0,0 +1,40 @@
+package stringcalculatorkatalyst;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.regex.Pattern;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class MultipleBracketAndSingleCharSeparatorFactoryTest {
+
+    private MultipleBracketAndSingleCharSeparatorFactory factory;
+
+    @Before
+    public void setUp() {
+        factory = new MultipleBracketAndSingleCharSeparatorFactory();
+    }
+
+    public Object[][] parametersForCreatePatternFromProvidedInput() {
+        return new Object[][] {
+                {"[*]", Pattern.compile(";|\n|\\*")}
+        };
+    }
+    @Test
+    @Parameters(method = "parametersForCreatePatternFromProvidedInput")
+    public void createPatternThatIncludeDefaultSeparatorFromProvidedInput(String string, Pattern pattern) {
+        assertThat(factory.createPattern(string), is(equalTo(pattern)));
+    }
+
+
+
+}
