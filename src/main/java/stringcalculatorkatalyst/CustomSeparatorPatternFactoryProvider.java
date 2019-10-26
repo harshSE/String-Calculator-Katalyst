b180d6a952afa660f950899d38b46d33a654d400
harshSE <h.rsh89@gmail.com>
2019-10-27 02:30:23 +0530

    STEP 8.11,  REFACTOR signal, provided proper name to patten ref

--- src/main/java/stringcalculatorkatalyst/CustomSeparatorPatternFactoryProvider.java
+++ src/main/java/stringcalculatorkatalyst/CustomSeparatorPatternFactoryProvider.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 public class CustomSeparatorPatternFactoryProvider {
 
     public CustomSeparatorPatternFactory get(String customSeparatorString) {
-        Pattern pattern1 = Pattern.compile("^(\\[.\\])+");
+        Pattern pattern = Pattern.compile("^(\\[.\\])+");
 
-        if(pattern1.matcher(customSeparatorString).matches()) {
+        if(pattern.matcher(customSeparatorString).matches()) {
             return new MultipleCommaAndSingleCharSeparatorFactory();
         } else {
             return null;
