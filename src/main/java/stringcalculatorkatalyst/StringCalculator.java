76bde15fe977c53b1117bac6a758ad1920d6aef6
harshSE <h.rsh89@gmail.com>
2019-10-26 12:09:24 +0530

    STEP 1.8, Refactor, remove duplication

--- src/main/java/stringcalculatorkatalyst/StringCalculator.java
+++ src/main/java/stringcalculatorkatalyst/StringCalculator.java
@@ -4,12 +4,6 @@ import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
     public int add(String stringNumber) {
-        if(parseInt(stringNumber) == 2) {
-            return 2;
-        } if(parseInt(stringNumber) == -1) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return parseInt(stringNumber);
     }
 }
