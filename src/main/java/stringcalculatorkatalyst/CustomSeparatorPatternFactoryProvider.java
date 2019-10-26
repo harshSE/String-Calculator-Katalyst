28c866a9a3c9a1db44568aba44bdb565b6a4ee78
harshSE <h.rsh89@gmail.com>
2019-10-27 02:26:52 +0530

    STEP 8.10,  GREEN signal, pass the failed test case

--- src/main/java/stringcalculatorkatalyst/CustomSeparatorPatternFactoryProvider.java
+++ src/main/java/stringcalculatorkatalyst/CustomSeparatorPatternFactoryProvider.java
@@ -1,8 +1,16 @@
 package stringcalculatorkatalyst;
 
+import java.util.regex.Pattern;
+
 public class CustomSeparatorPatternFactoryProvider {
-    
-    public CustomSeparatorPatternFactory get(String numbers) {
-        return null;
+
+    public CustomSeparatorPatternFactory get(String customSeparatorString) {
+        Pattern pattern1 = Pattern.compile("^(\\[.\\])+");
+
+        if(pattern1.matcher(customSeparatorString).matches()) {
+            return new MultipleCommaAndSingleCharSeparatorFactory();
+        } else {
+            return null;
+        }
     }
 }
