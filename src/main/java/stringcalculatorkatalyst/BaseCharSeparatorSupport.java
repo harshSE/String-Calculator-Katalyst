f10041e3ce61920e1d24aab0e4c3c2d99d4fd091
harshSE <h.rsh89@gmail.com>
2019-10-27 03:19:23 +0530

    STEP 8.21,  REFACTOR signal, create base support class to avoid duplication

---
+++ src/main/java/stringcalculatorkatalyst/BaseCharSeparatorSupport.java
@@ -0,0 +1,35 @@
+package stringcalculatorkatalyst;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public abstract class BaseCharSeparatorSupport {
+
+    private HashSet<Character> escapeCharacters;
+
+
+    public BaseCharSeparatorSupport() {
+        escapeCharacters = new HashSet<>(asList('\\','^','*', '$','.','|','?','+','(',')','[',']','{','}'));
+    }
+
+    protected String createRegex(List<String> separators) {
+        StringBuilder newCustomSeparator = new StringBuilder();
+        for (String separator : separators) {
+            char customSeparatorChar = separator.charAt(0);
+
+            newCustomSeparator.append('|');
+            if(escapeCharacters.contains(customSeparatorChar)) {
+
+                for(int index = 0; index < separator.length(); index++) {
+                    newCustomSeparator.append("\\").append(customSeparatorChar);
+                }
+            } else {
+                newCustomSeparator.append(customSeparatorChar);
+            }
+        }
+
+        return ",|\n" + newCustomSeparator;
+    }
+}
