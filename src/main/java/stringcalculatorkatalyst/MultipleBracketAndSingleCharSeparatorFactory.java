5a984d9ab298c66f964ebdf1b6a17da20abf9df0
harshSE <h.rsh89@gmail.com>
2019-10-27 02:51:13 +0530

    STEP 8.17,  RED signal, test fail for providing patten

--- src/main/java/stringcalculatorkatalyst/MultipleBracketAndSingleCharSeparatorFactory.java
+++ src/main/java/stringcalculatorkatalyst/MultipleBracketAndSingleCharSeparatorFactory.java
@@ -1,4 +1,9 @@
 package stringcalculatorkatalyst;
 
+import java.util.regex.Pattern;
+
 public class MultipleBracketAndSingleCharSeparatorFactory implements CustomSeparatorPatternFactory{
+    public Pattern createPattern(String string) {
+        return null;
+    }
 }
