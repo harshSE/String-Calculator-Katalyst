2845032f5ab6d563121d5c8b020552136c480523
harshSE <h.rsh89@gmail.com>
2019-10-26 14:35:24 +0530

    STEP 1.32, GREEN signal , pass the failed test case

--- src/main/java/stringcalculatorkatalyst/StringCalculator.java
+++ src/main/java/stringcalculatorkatalyst/StringCalculator.java
@@ -3,9 +3,14 @@ package stringcalculatorkatalyst;
 import java.util.Objects;
 
 import static java.lang.Integer.parseInt;
+import static java.util.Objects.isNull;
 
 public class StringCalculator {
-    public int add(String stringNumber) {
+    public int add(String stringNumber) throws IllegalArgumentException{
+
+        if(isNull(stringNumber)) {
+           throw new IllegalArgumentException("Null string not allowed");
+        }
 
         if(stringNumber.trim().equals("")) {
             return 0;
