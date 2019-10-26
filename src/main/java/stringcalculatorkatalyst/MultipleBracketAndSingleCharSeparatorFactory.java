8a5cd6ea038ae3da35ac1bd2c6b0780951d9856e
harshSE <h.rsh89@gmail.com>
2019-10-27 03:04:13 +0530

    STEP 8.18,  REFACTOR signal

--- src/main/java/stringcalculatorkatalyst/MultipleBracketAndSingleCharSeparatorFactory.java
+++ src/main/java/stringcalculatorkatalyst/MultipleBracketAndSingleCharSeparatorFactory.java
@@ -17,11 +17,7 @@ public class MultipleBracketAndSingleCharSeparatorFactory implements CustomSepar
     }
 
     public Pattern createPattern(String customSeparatorString) {
-
-        List<String> separators = new ArrayList<>();
-
-
-        extractSeparator(customSeparatorString, separators);
+        List<String> separators = extractSeparator(customSeparatorString);
 
 
         String regexString = createRegex(separators);
@@ -29,13 +25,16 @@ public class MultipleBracketAndSingleCharSeparatorFactory implements CustomSepar
 
     }
 
-    private void extractSeparator(String customSeparatorString, List<String> separators) {
+    private List<String> extractSeparator(String customSeparatorString) {
+        List<String> separators = new ArrayList<>();
         for(int index = 0; index < customSeparatorString.length(); index+=3) {
             if(customSeparatorString.charAt(index) == '[' && customSeparatorString.charAt(index+2) == ']') {
                 char separator = customSeparatorString.charAt(index + 1);
                 separators.add(""+separator);
             }
         }
+
+        return separators;
     }
 
     private String createRegex(List<String> separators) {
