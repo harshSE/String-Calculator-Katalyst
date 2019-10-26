c0638de2200cd50f13a3ff196ca1e12e9400b85b
harshSE <h.rsh89@gmail.com>
2019-10-26 13:59:11 +0530

    STEP 1.25, GREEN signal, pass the failed test case

--- src/main/java/stringcalculatorkatalyst/StringCalculator.java
+++ src/main/java/stringcalculatorkatalyst/StringCalculator.java
@@ -22,6 +22,6 @@ public class StringCalculator {
     }
 
     private int toInt(String stringNumber) {
-        return parseInt(stringNumber);
+        return parseInt(stringNumber.trim());
     }
 }
