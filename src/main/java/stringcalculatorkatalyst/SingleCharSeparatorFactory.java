c5a4fa4d32bebeb615bc92429c1a744180afd04c
harshSE <h.rsh89@gmail.com>
2019-10-27 03:25:32 +0530

    STEP 8.23,  GREEN signal, pass failed test case

--- src/main/java/stringcalculatorkatalyst/SingleCharSeparatorFactory.java
+++ src/main/java/stringcalculatorkatalyst/SingleCharSeparatorFactory.java
@@ -1,9 +1,16 @@
 package stringcalculatorkatalyst;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
-public class SingleCharSeparatorFactory implements CustomSeparatorPatternFactory {
+public class SingleCharSeparatorFactory extends BaseCharSeparatorSupport implements CustomSeparatorPatternFactory {
     public Pattern createPattern(String customSeparatorString) {
-        return null;
+
+        List<String> separators = new ArrayList<>();
+        separators.add(customSeparatorString);
+
+        String regexString = createRegex(separators);
+        return Pattern.compile(regexString);
     }
 }
