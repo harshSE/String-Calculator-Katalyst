0adf20d0f59ce09011c075bed5d5e05c30ec41c8
harshSE <h.rsh89@gmail.com>
2019-10-27 00:35:02 +0530

    STEP 8.4,  REFACTOR, perform parsing with regex

--- src/main/java/stringcalculatorkatalyst/CalInputStringParser.java
+++ src/main/java/stringcalculatorkatalyst/CalInputStringParser.java
@@ -24,21 +24,11 @@ class CalInputStringParser {
 
         String val = stringNumber.trim();
 
-        Stream<String> stringNumberStream = null;
+        Stream<String> stringNumberStream;
 
 
         if(stringNumber.startsWith("//")) {
-            stringNumberStream = splitWithPatten(stringNumber);
-        }
-
-        if(Objects.nonNull(stringNumberStream)) {
-            return stringNumberStream.map(String::trim).mapToInt(Integer::parseInt).toArray();
-        }
-
-        if(stringNumber.startsWith("//[")) {
-            stringNumberStream = splitWithArbitraryLengthOfCustomSeparator(val);
-        } else if(stringNumber.startsWith("//")) {
-            stringNumberStream = splitWithCustomSeparator(val);
+            stringNumberStream = splitWithPatten(val);
         } else {
             stringNumberStream = pattern.splitAsStream(val);
         }
@@ -49,28 +39,30 @@ class CalInputStringParser {
     private Stream<String> splitWithPatten(String stringNumber) {
         int startIndexOfNewLineChar = stringNumber.indexOf("\n");
 
-        List<String> separators = new ArrayList<>();
-        for(int index = 2; index < startIndexOfNewLineChar; index+=3) {
-            if(stringNumber.charAt(index) == '[' && stringNumber.charAt(index+2) == ']') {
-                char separator = stringNumber.charAt(index + 1);
-                separators.add(""+separator);
-            } else {
-                break;
+        Pattern pattern1 = Pattern.compile("^(\\[.\\])+");
+
+        if(pattern1.matcher(stringNumber.substring(2,startIndexOfNewLineChar)).matches()) {
+            List<String> separators = new ArrayList<>();
+            for(int index = 2; index < startIndexOfNewLineChar; index+=3) {
+                if(stringNumber.charAt(index) == '[' && stringNumber.charAt(index+2) == ']') {
+                    char separator = stringNumber.charAt(index + 1);
+                    separators.add(""+separator);
+                }
             }
+
+            String regex = createRegex(separators);
+            Pattern pattern = Pattern.compile(regex);
+
+            String substring = stringNumber.substring(startIndexOfNewLineChar + 1);
+
+            return  pattern.splitAsStream(substring);
+        } else if(stringNumber.startsWith("//[") && Pattern.compile("^\\[[^\\]]+\\]").matcher(stringNumber.substring(2,startIndexOfNewLineChar)).matches()) {
+            return splitWithArbitraryLengthOfCustomSeparator(stringNumber);
+        } else if(Pattern.compile("^.").matcher(stringNumber.substring(2,startIndexOfNewLineChar)).matches()) {
+            return splitWithCustomSeparator(stringNumber);
+        } else {
+            throw new IllegalArgumentException("Invalid expression provided after //");
         }
-
-        if(separators.isEmpty()) {
-            return null;
-        }
-
-        String regex = createRegex(separators);
-        Pattern pattern = Pattern.compile(regex);
-
-        String substring = stringNumber.substring(startIndexOfNewLineChar + 1);
-
-        return  pattern.splitAsStream(substring);
-
-
 
     }
 
@@ -78,43 +70,14 @@ class CalInputStringParser {
 
     private Stream<String> splitWithCustomSeparator(String stringNumber) {
         int endIndexOfNewLineChar = stringNumber.indexOf("\n");
-
-        if (isNoCharBetweenDoubleSlashAndNewChar(endIndexOfNewLineChar)) {
-            throw new ValidationException("No separator provided");
-        } else if (isMoreThanOneCharBetweenDoubleSlashAndNewChar(endIndexOfNewLineChar)) {
-            throw new ValidationException("Only single character is allowed as custom separator");
-        }
         return splitWithCustomSeparator(stringNumber, endIndexOfNewLineChar, 2, endIndexOfNewLineChar);
     }
 
-    private boolean isMoreThanOneCharBetweenDoubleSlashAndNewChar(int endIndexOfNewLineChar) {
-        return endIndexOfNewLineChar != 3;
-    }
-
-    private boolean isNoCharBetweenDoubleSlashAndNewChar(int endIndexOfNewLineChar) {
-        return endIndexOfNewLineChar == 2;
-    }
-
     private Stream<String> splitWithArbitraryLengthOfCustomSeparator(String stringNumber) {
-
         int endIndexOfNewLineChar = stringNumber.indexOf("\n");
         int endIndex = endIndexOfNewLineChar - 1;
-        char charAtEndIndex = stringNumber.charAt(endIndex);
+        return splitWithCustomSeparator(stringNumber, endIndexOfNewLineChar, 3, endIndex);
 
-        if (charAtEndIndex == '[') {
-            return splitWithCustomSeparator(stringNumber);
-        } else if (charAtEndIndex == ']' && isAnyCharsBetweenBracket(endIndex)) {
-            throw new ValidationException("No separator provided between bracket");
-        }  else if (charAtEndIndex != ']') {
-            throw new ValidationException("No closing bracket provided");
-        } else {
-            return splitWithCustomSeparator(stringNumber, endIndexOfNewLineChar, 3, endIndex);
-        }
-
-    }
-
-    private boolean isAnyCharsBetweenBracket(int indexOfClosingBracket) {
-        return indexOfClosingBracket - 2 == 1;
     }
 
     private Stream<String> splitWithCustomSeparator(String stringNumber, int endIndexOfNewLineChar, int beginIndex, int endIndex) {
