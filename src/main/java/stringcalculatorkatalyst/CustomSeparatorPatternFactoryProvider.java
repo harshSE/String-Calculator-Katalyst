5823dd97ec0d23170acde8400bf149e4a3a9f565
harshSE <h.rsh89@gmail.com>
2019-10-27 02:12:54 +0530

    STEP 8.7,  RED signal, compilation error removed for custom separator pattern factory provider test

---
+++ src/main/java/stringcalculatorkatalyst/CustomSeparatorPatternFactoryProvider.java
@@ -0,0 +1,9 @@
+package stringcalculatorkatalyst;
+
+public class CustomSeparatorPatternFactoryProvider {
+
+
+    public CustomSeparatorPatternFactory get(String s) {
+        throw new UnsupportedOperationException("Currently not supported");
+    }
+}
