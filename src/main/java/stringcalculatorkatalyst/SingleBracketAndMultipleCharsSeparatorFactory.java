e19c9c747dd17949308b0b228d0fa3e4da66fce7
harshSE <h.rsh89@gmail.com>
2019-10-27 03:08:31 +0530

    STEP 8.20,  RED signal, test case failed for single bucket and multiple char separator

--- src/main/java/stringcalculatorkatalyst/SingleBracketAndMultipleCharsSeparatorFactory.java
+++ src/main/java/stringcalculatorkatalyst/SingleBracketAndMultipleCharsSeparatorFactory.java
@@ -1,4 +1,9 @@
 package stringcalculatorkatalyst;
 
+import java.util.regex.Pattern;
+
 public class SingleBracketAndMultipleCharsSeparatorFactory implements CustomSeparatorPatternFactory{
+    public Pattern createPattern(String customSeparatorString) {
+        return null;
+    }
 }
